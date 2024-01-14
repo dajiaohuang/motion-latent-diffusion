f8c75bdc794f741f896c16f3870898088be28488
Wu Shuwen <108231307+dajiaohuang@users.noreply.github.com>
2024-01-14 12:38:38 +0800

    Update prepare_clip.sh

--- prepare/prepare_clip.sh
+++ prepare/prepare_clip.sh
@@ -1,4 +1,5 @@
 cd deps/
 git lfs install
-git clone https://huggingface.co/openai/clip-vit-large-patch14
+# git clone https://huggingface.co/openai/clip-vit-large-patch14
+git clone https://hf-mirror.com/openai/clip-vit-large-patch14
 cd ..
